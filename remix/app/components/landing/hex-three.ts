a581dc197aa8e04e047b78b17caf7f51ab071ed8
JagritGumber <gumberjagrit@gmail.com>
2026-06-30 04:29:03 +0530

    fix: Z rotation -0.2

--- remix/app/components/landing/hex-three.ts
+++ remix/app/components/landing/hex-three.ts
@@ -34,7 +34,7 @@ export function createHexCoin(container: HTMLElement) {
   // Static front view
   coin.rotation.x = 1.5 + Math.PI
   coin.rotation.y = 0
-  coin.rotation.z = 0.4
+  coin.rotation.z = -0.2
   
   scene.add(coin)
   
