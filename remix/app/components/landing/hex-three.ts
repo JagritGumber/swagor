99c8d8e915dd03816653d6b677d8e6d036213eaf
JagritGumber <gumberjagrit@gmail.com>
2026-06-30 04:42:35 +0530

    fix: mirror hex with scale.x = -1

--- remix/app/components/landing/hex-three.ts
+++ remix/app/components/landing/hex-three.ts
@@ -34,7 +34,7 @@ export function createHexCoin(container: HTMLElement) {
   // Static front view
   coin.rotation.x = 1.5 + Math.PI
   coin.rotation.y = 0
-  coin.rotation.z = -0.4
+  coin.scale.x = -1
   
   scene.add(coin)
   
