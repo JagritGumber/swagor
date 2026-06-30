57b9048faecdc2379ff9208bb4f3375df249965f
JagritGumber <gumberjagrit@gmail.com>
2026-06-30 06:46:59 +0530

    fix: hexagon z=-0.5

--- remix/app/components/landing/hex-three.ts
+++ remix/app/components/landing/hex-three.ts
@@ -54,7 +54,7 @@ export function createHexCoin(container: HTMLElement) {
   })
   const coin = new THREE.Mesh(geometry, material)
   geometry.center()
-  coin.position.set(0, 1.5, -1)
+  coin.position.set(0, 1.5, -0.5)
   coin.castShadow = true
   
   scene.add(coin)
