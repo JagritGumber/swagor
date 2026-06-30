9e683d64a6cd01daccf08e9150d8ea392a97711f
JagritGumber <gumberjagrit@gmail.com>
2026-06-30 06:18:49 +0530

    fix: move hexagon back z=-1

--- remix/app/components/landing/hex-three.ts
+++ remix/app/components/landing/hex-three.ts
@@ -54,7 +54,7 @@ export function createHexCoin(container: HTMLElement) {
   })
   const coin = new THREE.Mesh(geometry, material)
   geometry.center()
-  coin.position.set(0, 1.5, 0)
+  coin.position.set(0, 1.5, -1)
   coin.castShadow = true
   
   scene.add(coin)
