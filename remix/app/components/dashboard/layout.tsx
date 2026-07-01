15ef524fc05143b3ba04803842444884429ae931
JagritGumber <gumberjagrit@gmail.com>
2026-07-02 04:13:54 +0530

    Fix DashboardLayout to use Handle<> pattern for children rendering

--- remix/app/components/dashboard/layout.tsx
+++ remix/app/components/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import type { RemixNode } from 'remix/ui'
+import type { Handle, RemixNode } from 'remix/ui'
 import { Navbar } from '../navbar.tsx'
 import * as s from './style.ts'
 
@@ -6,12 +6,12 @@ interface DashboardLayoutProps {
   children: RemixNode
 }
 
-export function DashboardLayout(handle: DashboardLayoutProps) {
+export function DashboardLayout(handle: Handle<DashboardLayoutProps>) {
   return () => (
     <div mix={s.shell}>
       <Navbar />
       <div mix={s.content}>
-        {handle.children}
+        {handle.props.children}
       </div>
     </div>
   )
