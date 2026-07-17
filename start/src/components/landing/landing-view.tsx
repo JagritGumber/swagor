90b35f469023c3e06fc98eeb618afb634aaee8a5
JagritGumber <gumberjagrit@gmail.com>
2026-07-18 00:55:16 +0530

    fix: remove JudgmentPanel from live page, signals take full sidebar
    JudgmentPanel was showing irrelevant 'mind' log. Removed it.
    Signals panel now takes full right sidebar height.

--- start/src/components/landing/landing-view.tsx
+++ start/src/components/landing/landing-view.tsx
@@ -199,14 +199,7 @@ export function LandingView({ assets, activeAsset: initialAsset, page = 'portfol
             </div>
           </div>
 
-          <div className="flex flex-col overflow-hidden">
-            <div className="min-h-0 flex-1 overflow-hidden">
-              <JudgmentPanel log={mindLog} updatedAt={updatedAt} asset={activeAsset} />
-            </div>
-            <div className="h-72 shrink-0 border-t border-border-default">
-              <SignalsPanel />
-            </div>
-          </div>
+          <SignalsPanel />
         </div>
       ) : (
         <>
